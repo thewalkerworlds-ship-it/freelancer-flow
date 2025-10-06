26c484cf6d3a78dd33fc97ee54d502c63de7d4a4
thewalkerworlds-ship-it <thewalkerworlds@gmail.com>
2025-10-06 22:51:19 +0530

    fix: Set Vercel root directory

---
+++ frontend/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// PASTE YOUR COPIED CONFIGURATION OBJECT HERE
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
